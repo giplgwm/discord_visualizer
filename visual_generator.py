d2b675f17171153c30483674ea30f1dab7cde76d
Desktop <joshlgwm@gmail.com>
2023-10-26 03:34:03 -0400

    updated layout

--- visual_generator.py
+++ visual_generator.py
@@ -1,11 +1,12 @@
 import discord
 import os
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 async def get_user_activities(guild):
     """Get a list of activities being done by non-bot members in a guild."""
-    activities = {}
+    activities = {'None': [],}
     for member in guild.members:
         if member.bot:
             continue
@@ -16,21 +17,33 @@ async def get_user_activities(guild):
             else:
                 activities[member.activity.name] = [member.name]
         else:
-            if 'None' in activities:
-                activities['None'].append(member.name)
-            else:
-                activities['None'] = [member.name]
+            activities['None'].append(member.name)
     members_in_each = [len(members) for members in activities.values()]
     return activities, members_in_each
 
 
 def create_pie_chart(activities, guild, members_in_each):
-    """Create a pie chart representing what the members of a guild are playing."""
+    """Create a pie chart representing what the members of a guild are playing. Make an annotation to show
+    people who aren't playing anything"""
+    members_with_no_activity = activities['None']
+    del members_in_each[0]
+    del activities['None']
     fig = px.pie(names=activities.keys(), values=members_in_each,
                  title=f"Whats being played in {guild.name}?", labels={'values': 'Members Playing'},
-                 template='plotly_dark')
-    fig.update_traces(textposition='inside', textinfo='value+label', textfont_size=20, hoverinfo='label+percent')
-    fig.update_layout(showlegend=True, title_x=0.5, titlefont_size=24)
+                 template='plotly_dark', color_discrete_sequence=px.colors.sequential.Purples_r)
+    fig.update_traces(textposition='inside', textinfo='value+label', textfont=dict(color='white'))
+    fig.update_layout(uniformtext_minsize=14, uniformtext_mode='show')
+    fig.update_layout(showlegend=False, title_x=0.5, titlefont_size=18)
+    fig.update_layout(margin=dict(l=20, r=20, t=80, b=60))
+    fig.add_annotation(dict(font=dict(color='White', size=15),
+                            x=0,
+                            y=-0.12,
+                            showarrow=False,
+                            text=f"Members doing nothing: {len(members_with_no_activity)}",
+                            textangle=0,
+                            xanchor='left',
+                            xref="paper",
+                            yref="paper"))
     fig.write_image(f"images/{guild.id}.png")
 
 
