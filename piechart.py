6efdf440d3911cf14eafac16194b46bc802bb58d
Desktop <joshlgwm@gmail.com>
2023-10-30 12:21:16 -0400

    Linting

--- piechart.py
+++ piechart.py
@@ -1,10 +1,14 @@
+from io import BytesIO
 import discord
 import plotly.express as px
-import plotly.graph_objects as go
-from io import BytesIO
+
+"""This module defines the charts that will be sent by the discord bot."""
 
 
 class ActivityChart:
+    """Parent class for charts made from discord guild member activity data. Should be subclassed to add new
+    chart types."""
+
     def __init__(self, channel: discord.TextChannel):
         self.channel = channel
         self.guild = channel.guild
@@ -31,7 +35,6 @@ class ActivityChart:
     def _create_visual(self):
         """Create a visual representation of the data gathered. Left empty in parent class, this is the only method
         that must be added in a child class to define the type of chart created."""
-        ...
 
     def _im_to_bytesio(self):
         """Turn the fig created into a BytesIO object, for passing into discord.File()"""
@@ -39,7 +42,9 @@ class ActivityChart:
 
 
 class PieChart(ActivityChart):
-    """Represents a Pie Chart for a given discord guild's activity. Use the image attribute to access a bytesIO object or .fig for the plotly figure"""
+    """Represents a Pie Chart for a given discord guild's activity.
+    Use the image attribute to access a bytesIO object or .fig for the plotly figure."""
+
     def __init__(self, channel: discord.TextChannel):
         super().__init__(channel)
 
@@ -54,27 +59,28 @@ class PieChart(ActivityChart):
                           title=f"Whats being played in {self.guild.name}?",
                           template='plotly_dark',
                           color_discrete_sequence=px.colors.sequential.Purples_r)
-        self.fig.update_traces(textposition='inside', textinfo='value+label', textfont=dict(color='white'))
+        self.fig.update_traces(textposition='inside', textinfo='value+label', textfont={'color': 'White'})
         self.fig.update_layout(uniformtext_minsize=14,
                                uniformtext_mode='show',
                                showlegend=False,
                                title_x=0.5,
                                titlefont_size=18,
-                               margin=dict(l=20, r=20, t=80, b=60))
-        self.fig.add_annotation(dict(font=dict(color='White', size=15),
-                                     x=0,
-                                     y=-0.12,
-                                     showarrow=False,
-                                     text=f"Members doing nothing: {len(self.members_with_no_activity)}",
-                                     textangle=0,
-                                     xanchor='left',
-                                     xref="paper",
-                                     yref="paper"))
+                               margin={'l': 20, 'r': 20, 't': 80, 'b': 60, })
+        self.fig.add_annotation({'font': {'color': 'White', 'size': 15},
+                                 'x': 0,
+                                 'y': -0.12,
+                                 'showarrow': False,
+                                 'text': f"Members doing nothing: {len(self.members_with_no_activity)}",
+                                 'textangle': 0,
+                                 'xanchor': 'left',
+                                 'xref': "paper",
+                                 'yref': "paper"})
 
 
 class BarChart(ActivityChart):
     """Create a pie chart representing what the members of a guild are playing. Make an annotation to show
             people who aren't playing anything"""
+
     def __init__(self, channel):
         super().__init__(channel)
 
@@ -94,13 +100,13 @@ class BarChart(ActivityChart):
                                showlegend=False,
                                title_x=0.5,
                                titlefont_size=18,
-                               margin=dict(l=20, r=20, t=80, b=100))
-        self.fig.add_annotation(dict(font=dict(color='White', size=15),
-                                     x=0,
-                                     y=-0.4,
-                                     showarrow=False,
-                                     text=f"Members doing nothing: {len(self.members_with_no_activity)}",
-                                     textangle=0,
-                                     xanchor='left',
-                                     xref="paper",
-                                     yref="paper"))
+                               margin={'l': 20, 'r': 20, 't': 80, 'b': 100, })
+        self.fig.add_annotation({'font': {'color': 'White', 'size': 15},
+                                 'x': 0,
+                                 'y': -0.4,
+                                 'showarrow': False,
+                                 'text': f"Members doing nothing: {len(self.members_with_no_activity)}",
+                                 'textangle': 0,
+                                 'xanchor': 'left',
+                                 'xref': "paper",
+                                 'yref': "paper"})
